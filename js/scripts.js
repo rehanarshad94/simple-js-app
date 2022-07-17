c31448b1fb483c654d7a3d18a6dd34846b13d96a
Rehan Arshad <rehanarshad94@yahoo.com>
2022-07-17 14:17:17 -0500

    Loop
    added loop and document.write for the code to show on DOM for each pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//Pokemon JS Project
+
 let pokemonList = [ 
 
     {
@@ -18,4 +20,16 @@ let pokemonList = [
      }
 ] ;
 
+
+//Loop for pokemonList 
+for (let i=0; i<pokemonList.length; i++) {
+   //the condition to which the height has to be within for output to be shown as 'big' or 'small' 
+   if (pokemonList[i].height<=1 && pokemonList[i].height>=0) {
+      //writes text for which pokemons have height below the value as "small"
+      document.write('<br>' + pokemonList[i].name + ' (height:' + pokemonList[i].height + ') -"Small Pokemon!" '); 
+   } else {
+      //writes text for which pokemons have height above the value as "big"
+      document.write('<br>' + pokemonList[i].name + ' (height:' +pokemonList[i].height + ') -"Big Pokemon!" '); 
+   }
+}
 
