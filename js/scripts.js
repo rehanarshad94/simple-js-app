59a8cbb6932f5648098d5908dc29805a4a429318
Rehan Arshad <rehanarshad94@yahoo.com>
2022-07-30 18:49:02 -0500

    js
    updated js with 2 new functions. created and added button option for each pokemon with their names in each button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 //Pokemon JS Project
 
 
-//For console.log
 let pokemonListRepository = (function () {
 
 let pokemonList = [ 
@@ -25,60 +24,49 @@ let pokemonList = [
 ];
 
 
+
+
 function getAll () {
    return (pokemonList);
 }
 
-function add (pokemon) {
+function add(pokemon) {
    pokemonList.push(pokemon);
 }
 
+
+function addListItem(pokemon) {
+   let pokelist = document.querySelector('.pokemon-list');
+   let listItem = document.createElement('li');
+   let button = document.createElement('button');
+   button.innerText = pokemon.name;
+   button.classList.add('button');
+   button.addEventListener('click', showDetails (pokemon));
+   listItem.appendChild(button); 
+   pokelist.appendChild(listItem);
+}
+
+function showDetails(pokemon) {
+   console.log(pokemon);
+}
+
+
 return {
    getAll: getAll,
-   add: add
+   add:add,
+   addListItem: addListItem,
 }
+
+
 
 })();
 
-
-
-pokemonListRepository.add({name:'Mewtwo', height:2, type:['psychic']});
-pokemonListRepository.add({name:'Jigglypuff', height:0.5, type:['fairy', 'normal']});
+pokemonListRepository.add({name:'Mewtwo',height:2, type:['psychic']});
+pokemonListRepository.add({name:'JigglyPuff',height:0.5, type:['fairy', 'normal']});
 console.log(pokemonListRepository.getAll());
 
-
-
-
-
-
-//For document.write
-let pokemonList = [ 
-
-   {
-       name: 'Blastoise',
-       height: 1.6,
-       type: [ 'water' ]
-    } ,   
-    {
-       name: 'Pikachu',
-       height: 0.4,
-       type: [ 'electric' ]
-    },
-
-    {
-       name: 'Charizard',
-       height: 1.7,
-       type: [ 'fire', 'flying']
-    }
-];
-
-//forEach Loop
-pokemonList.forEach(function (pokemonList) {
-   if (pokemonList.height <= 1 && pokemonList.height >= 0 ) {
-      document.write('<br>' + pokemonList.name + ' (height:' + ' ' + pokemonList.height + ') -"Small Pokemon');
-   } else {
-      document.write('<br>' + pokemonList.name + ' (height:' + ' ' + pokemonList.height + ') -"Big Pokemon');
-   }
+pokemonListRepository.getAll().forEach(function (pokemon) {
+   pokemonListRepository.addListItem(pokemon);
 });
 
 
@@ -102,19 +90,18 @@ pokemonList.forEach(function (pokemonList) {
 
 
 
-/*
-//For Loop 
-for (let i=0; i<pokemonList.length; i++) {
-   //the condition to which the height has to be within for output to be shown as 'big' or 'small' 
-   if (pokemonList[i].height<=1 && pokemonList[i].height>=0) {
-      //writes text for which pokemons have height below the value as "small"
-      document.write('<br>' + pokemonList[i].name + ' (height:' + pokemonList[i].height + ') -"Small Pokemon!" '); 
-   } else {
-      //writes text for which pokemons have height above the value as "big"
-      document.write('<br>' + pokemonList[i].name + ' (height:' +pokemonList[i].height + ') -"Big Pokemon!" '); 
-   }
-};
-*/
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
