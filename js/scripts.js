fb81d6392aec82debbe058ac095910ce78b879a5
Rehan Arshad <rehanarshad94@yahoo.com>
2022-08-06 21:42:35 -0500

    JS update
    Selected h1 to add title to DOM.
    
    Removed all pokemonvobjects inside pokemonList array.
    
    Added 'event' as a argument inside the addListItem function in the addEventListener.
    
    added LoadList as a function to load the list of the pokemon and added loadDetails to provide specific details for each pokemon.
    
    added loadList, loadDetails, showDetails im the return function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,15 @@
 //Pokemon JS Project
 
+//Title 
+let h1Title = document.querySelector('h1');
+h1Title.innerText = 'Pokedex';
+document.write(h1Title);
 
-let pokemonListRepository = (function () {
 
-let pokemonList = [ 
 
-    {
-        name: 'Blastoise',
-        height: 1.6,
-        type: [ 'water' ]
-     } ,   
-     {
-        name: 'Pikachu',
-        height: 0.4,
-        type: [ 'electric' ]
-     },
-
-     {
-        name: 'Charizard',
-        height: 1.7,
-        type: [ 'fire', 'flying']
-     }
-];
+let pokemonListRepository = (function () {  
+let pokemonList = [];
+let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
 
 
@@ -41,35 +29,101 @@ function addListItem(pokemon) {
    let button = document.createElement('button');
    button.innerText = pokemon.name;
    button.classList.add('button');
-   button.addEventListener('click', function() {
+   button.addEventListener('click', function(event) {
       showDetails(pokemon)
    });
    listItem.appendChild(button); 
    pokelist.appendChild(listItem);
 }
 
-function showDetails(pokemon) {
-   console.log(pokemon);
-}
+
+
+function loadList() {
+   return fetch(apiUrl).then(function (response) {
+     return response.json();//promise
+   }).then(function (json) {
+     json.results.forEach(function (item) {
+       let pokemon = {
+         name: item.name,
+         detailsUrl: item.url
+       };//response to promise
+       add(pokemon);
+       console.log(pokemon); //adds all pokemon in console
+     });
+   }).catch(function (e) {
+     console.log(e);//error
+   })
+ }
+
+
+ function loadDetails(item) {
+   let url = item.detailsUrl;
+   return fetch(url).then(function (response) {
+     return response.json();//promise
+   }).then(function (details) {
+     // Now we add the details to the item
+     item.imageUrl = details.sprites.front_default;
+     item.height = details.height;
+     item.type = details.types; //reponse to promise
+   }).catch(function (e) {
+     console.log(e); //error
+   });
+ }
+
+
+
+ function showDetails(pokemon) {
+   pokemonListRepository.loadDetails(pokemon).then(function () {
+     console.log(pokemon);
+   });
+ }
 
 
 return {
    getAll: getAll,
    add:add,
    addListItem: addListItem,
+   loadList: loadList,
+   loadDetails: loadDetails,
+   showDetails: showDetails
 }
-
 
 
 })();
 
-pokemonListRepository.add({name:'Mewtwo',height:2, type:['psychic']});
-pokemonListRepository.add({name:'JigglyPuff',height:0.5, type:['fairy', 'normal']});
-console.log(pokemonListRepository.getAll());
+//pokemonListRepository.add({name:'Mewtwo',height:2, type:['psychic']});
+//pokemonListRepository.add({name:'JigglyPuff',height:0.5, type:['fairy', 'normal']});
+//console.log(pokemonListRepository.getAll());
 
+
+pokemonListRepository.loadList().then(function () {
 pokemonListRepository.getAll().forEach(function (pokemon) {
    pokemonListRepository.addListItem(pokemon);
+   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
